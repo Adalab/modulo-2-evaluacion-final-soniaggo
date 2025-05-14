46cd3e96def7f18b9d9babb8d189f8a67616686f
soniaggo <sonigit config --global user.email lamary@gmail.com>
2025-05-14 22:32:03 +0200

    add js

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
-'use strict'
+'use strict';
+const productListMust = document.querySelector(".js_ul2")
+const url = "https://fakestoreapi.com/products";
+
+let productsList =[];
+
+
+
+function paintProductsList(productsList , productListMust) {
+
+productsList.forEach((products))
+    if ( productList!= undefined) {
+        productListMust.innerHTML += `<li>
+        <img src="${products.image}" alt="${products.title}">  
+        h4>${products.title}</h4> 
+        <p>${products.price}</p>
+    <button>"add to cart"</button>
+        </li>`;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+fetch(url)
+.then (response => response.json())
+.then(data => {
+    console.log(data);
+    productsList = data.productsList;
+    
+paintProductsList(productsList, productListMust);
+
+});

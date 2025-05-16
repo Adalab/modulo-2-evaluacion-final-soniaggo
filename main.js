8b11efb7a75b24a9bcc83c86007d1a127804e921
soniaggo <sonigit config --global user.email lamary@gmail.com>
2025-05-16 19:55:23 +0200

    add js

--- main.js
+++ main.js
@@ -1,28 +1,63 @@
 'use strict';
-const productListMust = document.querySelector(".js_ul2")
+const productListMust = document.querySelector(".js_ul2");
+const productListAddCard = document.querySelector(".js_ul1")
+const buttonSearch = document.querySelector(".js_buttonsearch");
+const inputSearch = document.querySelector(".js_searchinput");
+
 const url = "https://fakestoreapi.com/products";
 
 let productsList =[];
+let productscard =[];
 
 
 
-function paintProductsList(productsList , productListMust) {
+function renderProductsList(productsList , productListMust) {
 
-productsList.forEach((products))
-    if ( productList!= undefined) {
-        productListMust.innerHTML += `<li>
-        <img src="${products.image}" alt="${products.title}">  
-        h4>${products.title}</h4> 
-        <p>${products.price}</p>
-    <button>"add to cart"</button>
-        </li>`;
-    }
+for( let product of productsList){
+   const productId = product.id;
+    const li = document.createElement("li");
+    li.classList.add("card");
+    li.innerHTML = `
+     <img src="${product.image}" alt="${product.title}">
+     <h4>${product.title}</h4>
+     <p>Price: $${product.price}</p>
+     <button class=" js_buttonAdd button-card" data-product-id="${productId}">Add to Card</button>
+   `;
+    productListMust.appendChild(li);
 
 }
 
 
+};
+const buttonAdd = document.querySelectorAll(".js_buttonAdd");
+
+function rendercard (product, productListAddCard){
+     const li = document.createElement("li");
+    li.classList.add("card");
+    li.classList.add("add");
+    li.innerHTML = `
+     <img src="${product.image}" alt="${product.title}">
+     <h4>${product.title}</h4>
+     <p>Price: $${product.price}</p>
+     <button class=" js_buttonAdd button-card" data-product-id="${product.id}">remove</button>
+   `;
+    productListAddCard.appendChild(li);
+
+}
+
+function handleclicksearch(event){
+event.preventDefault();
+let searchValue = inputSearch.value.toLowerCase();
+let filteredProducts = productsList.filter(product => {
+    return product.title.toLowerCase().includes(searchValue);
+   
+    } );
+    productListMust.innerHTML='';
+  renderProductsList(filteredProducts,productListMust);
+  
 
 
+};
 
 
 
@@ -34,8 +69,14 @@ fetch(url)
 .then (response => response.json())
 .then(data => {
     console.log(data);
-    productsList = data.productsList;
-    
-paintProductsList(productsList, productListMust);
+    productsList = data;
+  
+renderProductsList(productsList,productListMust);
+rendercard(productscard,productListMust);
 
-});
+
+
+    });
+
+
+buttonSearch.addEventListener("click" ,handleclicksearch);
